6ceb19878009041d0265080f87606b010f192ccf
CRAZY <162934782+Djdhsjhhhsb@users.noreply.github.com>
2024-04-17 22:04:59 +0200

    Add files via upload

---
+++ OWNER.py
@@ -0,0 +1,10 @@
+# متطلبات التنصيب #
+OWNER = ["CRAZ_UP", "CRAZ_UP"]
+OWNER_NAME = "👿☠️ᏨᎡᎯ ᏃᎽ⸙ꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋ⧚ڪَٰـُـــٰٓريَٰـُـٰٓـزيَّٰ"
+BOT_TOKEN = "6893831227:AAG3UR2S2nZlOj-w17d2gDExQ5tKUuPn4W0"
+DATABASE = "mongodb+srv://fraon:programmerfraon@fr3on.tjonyue.mongodb.net/?retryWrites=true&w=majority"
+CHANNEL = "https://t.me/CR_OL_Q"
+GROUP = "https://t.me/TEM_CRAZY"
+VIDEO = "https://t.me/CAPTANML/13"
+LOGS = "SAGAL_botk"
+
